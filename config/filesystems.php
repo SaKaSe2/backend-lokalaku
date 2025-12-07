bb4fd7e820679a7df97a867f4185174227ab5449
SaKaSe2 <gmrikza05@gmail.com>
2025-12-07 22:39:01 +0700

    UP

--- config/filesystems.php
+++ config/filesystems.php
@@ -28,14 +28,15 @@ return [
     |
     */
 
- 'disks' => [
-    'public' => [
-        'driver' => 'local',
-        'root' => storage_path('app/public'),
-        'url' => env('APP_URL').'/storage',
-        'visibility' => 'public',
-        'throw' => false,
-    ],
+    'disks' => [
+
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app/private'),
+            'serve' => true,
+            'throw' => false,
+            'report' => false,
+        ],
 
         'public' => [
             'driver' => 'local',
